f4f792c11962e5d7b3501dca3b547c10b1703466
Gavin Brown <gav@greycubes.net>
2023-03-25 22:28:32 +0200

    Updated readme with usage instructions. Updated download model script to match docker-compose volume mount location

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl https://download.pytorch.org/models/vgg19-dcbb9e9d.pth > vgg19-dcbb9e9d.pth
+curl https://download.pytorch.org/models/vgg19-dcbb9e9d.pth > ./data/models/vgg19-dcbb9e9d.pth
